3920ef6cc3015bee4b0c51911f9b9870223d659b
Devin Ivy <devin@bigroomstudios.com>
2016-05-17 17:23:20 -0400

    First commit, mmmmm

---
+++ README.md
@@ -0,0 +1,5 @@
+# haute-couture
+
+directory structure as hapi plugin API
+
+[![Build Status](https://travis-ci.org/devinivy/haute.svg?branch=master)](https://travis-ci.org/devinivy/haute) [![Coverage Status](https://coveralls.io/repos/devinivy/haute/badge.svg?branch=master&service=github)](https://coveralls.io/github/devinivy/haute?branch=master)
---
+++ test/index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// Load modules
+
+const Lab = require('lab');
+const Code = require('code');
+const Hapi = require('hapi');
+const HauteCouture = require('..');
+
+// Test shortcuts
+
+const lab = exports.lab = Lab.script();
+const describe = lab.describe;
+const it = lab.it;
+const expect = Code.expect;
+
+const internals = {};
+
+describe('HauteCouture', () => {
+
+    it('', (done) => {
+
+
+    });
+
+});
